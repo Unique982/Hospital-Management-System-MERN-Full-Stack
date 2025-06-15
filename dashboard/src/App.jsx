5343487d887200b9d91fdbcda6d183d0541ddb2a
Unique Neupane <khemrajneupane111@gmail.com>
2025-06-15 18:04:44 +0545

    dashborad

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -4,12 +4,13 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import AdminLayout from "layouts/admin";
 import { Provider } from "react-redux";
 import store from "store/store";
+import ProtectedRoute from "ProtectedRoute/ProtectedRoute";
 
 const App = () => {
   return (
     <Provider store={store}>
       <Routes>
-        <Route path="admin/*" element={<AdminLayout />} />
+        <Route path="admin/*" element={<ProtectedRoute>{<AdminLayout />}</ProtectedRoute>} />
 
         <Route path="/" element={<Navigate to="/admin" replace />} />
       </Routes>
---
+++ dashboard/src/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    window.location.href = "http://localhost:5173/login";
+    return null; 
+  }
+  return children;
+};
+
+export default ProtectedRoute;
